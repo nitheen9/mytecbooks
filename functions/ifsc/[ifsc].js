a2641a1778754b1b299b69889947b31a1710c953
nitheen9 <nitheen99666@outlook.com>
2026-08-18 22:28:12 +0530

    Update [ifsc].js

--- functions/ifsc/[ifsc].js
+++ functions/ifsc/[ifsc].js
@@ -213,7 +213,7 @@ function createPage(title, content) {
 <head>
 
     <meta charset="UTF-8">
-
+    <link rel="icon" type="image/png" href="/favicon.png">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
 
