c1158b14487d47585b95e40776c8b22065a16bf6
nitheen9 <nitheen99666@outlook.com>
2026-08-19 12:50:47 +0530

    Update [ifsc].js

--- functions/ifsc/[ifsc].js
+++ functions/ifsc/[ifsc].js
@@ -241,6 +241,15 @@ function createPage(title, description, content) {
     <link rel="icon"
           type="image/png"
           href="/favicon.png">
+          <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BP9YJW8LB9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BP9YJW8LB9');
+</script>
 
     <title>${escapeHtml(title)}</title>
 
